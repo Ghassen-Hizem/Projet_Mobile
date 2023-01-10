0a35f685813724925cbe68a1a4c7a192d9962d59
Hizem <ghassen.hizem@supcom.tn>
2023-01-10 20:05:39 +0100

    architecture setup

--- Backend/app.js
+++ Backend/app.js
@@ -6,11 +6,21 @@ const port = 3000;
 
 const config = require("./db");
 
-app.get('/users', (req, res) => {
-    res.send('Hello World!')
-  })
+const auth = require('./routes/router-auth');
+const users = require('./routes/router-users');
+
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use('/auth',auth);
+app.use('/users',users);
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
 
