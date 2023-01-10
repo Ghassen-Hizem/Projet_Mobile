fb3a52b65735b9540aa17c22a0a8356ad6d3738e
Hizem <ghassen.hizem@supcom.tn>
2023-01-10 22:26:22 +0100

    get requests update

--- Backend/controllers/projects.controller.js
+++ Backend/controllers/projects.controller.js
@@ -8,5 +8,31 @@ const getProjects = (req,res) => {
         }
         )
 }
+const getProject = (req,res) => {
+    const { id } = req.params;
+        connection.execute(
+            'SELECT * FROM `Projects` WHERE `ID_projet`=?',[id],
+            function(err,results) {
+                res.status(200).json({success : true , data: results})
+            }
+            )
+    }
 
-module.exports = getProjects
+const getTasks = (req,res)=>{
+    const { id } = req.params;
+    connection.execute(
+        'SELECT * FROM `Tache` WHERE `id_projet`=?',[id],
+        function(err,results) {
+            res.status(200).json({success : true , data: results})
+        }
+        )
+}
+
+
+module.exports = {
+    getProjects,
+    getProject,
+    getTasks,
+
+}
+
--- Backend/routes/router-project.js
+++ Backend/routes/router-project.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 
-const getProjects =  require('../controllers/projects.controller')
+const {getProjects,getProject, getTasks} =  require('../controllers/projects.controller')
 
 
 router.route('/').get(getProjects);
 
 
+
+router.route('/project/:id').get(getProject);
+
+router.route('/project/tasks/:id').get(getTasks);
 
 module.exports = router;
