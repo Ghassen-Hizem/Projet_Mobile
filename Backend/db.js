efa3b1b34dd5557876529f53dc0a092a152190de
Ghassen_Hizem <89670714+Ghassen-Hizem@users.noreply.github.com>
2023-01-16 23:05:40 +0100

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'debian-sys-maint',
-  password: 'Vs92Sp4nxNuXeuoP',
+  user: 'your username',
+  password: 'random password',
   database: 'Collaboration_App'
 })
 
